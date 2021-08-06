d9a93f469d64a53f880a39b731bfa7978259b569
Azir <thoromuerte@gmail.com>
2021-08-06 20:14:01 +0300

    feat:add eslint

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 
 import { App } from './App';
 
-import "./design/reset.css";
-import "./design/global.css";
+import './design/reset.css';
+import './design/global.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
